139b34617ab1d303716a151c31c7502fb715b9de
haiderali780 <muhammadhaiderali82002@>
2023-11-17 22:24:44 +0500

    Completed notes app functionality.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,36 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
+import NotesList from './components/NotesList';
+import CreateNotes from './components/CreateNotes';
+import { Note } from './models/note.model';
 import './App.css';
+import { Col, Container, Row } from 'react-bootstrap';
 
 function App() {
+  const [notes, setNotes] = useState<Note[]>([{
+    id: (new Date).toString(),
+    title: "Meetings",
+    text: " Schedule meeting with UI/UX Team",
+    color: "#dfdfdf",
+    date: (new Date).toString()
+  }]);
 
-  
 
   return (
-    <div className="App">
+    <>
       
-    </div>
+      <Container className="mt-5">
+        <Row>
+          <Col>
+            <NotesList notes={notes} setNotes={ setNotes}/>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <CreateNotes notes={notes} setNotes={ setNotes}/>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
 
